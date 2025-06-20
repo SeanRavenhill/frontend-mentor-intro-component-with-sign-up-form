7df32e2c4e5d544a4233f8a2db0849e9c90a9387
Sean Ravenhill <11776929+SeanRavenhill@users.noreply.github.com>
2025-06-20 16:26:55 +0100

    chore: format, clean, prep and exported data at constants/index.js refactor: prettier enforeced formating changes to index.html and App.jsx

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,25 +1,44 @@
-<body>
+export const introComponent = [
+    {
+        id: 0,
+        title: {
+            content: 'Learn to code by watching others',
+        },
+    },
+    {
+        id: 2,
+        text: {
+            content:
+                'See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but understanding how developers think is invaluable.',
+        },
+    },
+    {
+        id: 3,
+        cta: {
+            content: 'Try it free 7 days then $20/mo. thereafter',
+        },
+    },
+    {
+        id: 4,
+        form: {
+            placeholderValues: ['First Name ', 'Last Name', 'Email Address', 'Password'],
+            button: 'Claim your free trial ',
+            legal: 'By clicking the button, you are agreeing to our Terms and Services',
+        },
+    },
+];
 
-  Learn to code by watching others
-
-  See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
-  but understanding how developers think is invaluable. 
-
-  Try it free 7 days then $20/mo. thereafter
-
-  First Name 
-  Last Name 
-  Email Address
-  Password
-
-  Claim your free trial 
-
-  By clicking the button, you are agreeing to our Terms and Services
-  
-  <footer>
-    <p class="attribution">
-      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-      Coded by <a href="#">Your Name Here</a>.
-    </p>
-  </footer>
-</body>
+export const attribution = [
+    {
+        id: 0,
+        url: 'https://www.frontendmentor.io?ref=challenge',
+    },
+    {
+        id: 2,
+        url: 'https://github.com/SeanRavenhill',
+    },
+    {
+        id: 3,
+        url: 'https://github.com/SeanRavenhill/frontend-mentor-intro-component-with-sign-up-form',
+    },
+];
